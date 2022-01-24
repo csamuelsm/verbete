1c11273f51228b5294aab662da708a18e5122362
Samuel Santos <csamuelssm@gmail.com>
2022-01-24 15:22:58 -0300

    heroku server

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -76,7 +76,7 @@ function typing(l, socket) {
 
 $(document).ready(function(){
 
-    const socket = io();
+    const socket = io('https://verbete.herokuapp.com/');
 
     /*socket.on('connect', function(){
         const sessionID = socket.socket.sessionID
